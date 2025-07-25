8c07f92969cf7855e08cb48fe40e14d747e20667
samuelnmu <samuelnmu@gmail.com>
2025-07-25 18:07:06 +0300

    access control

--- django-models/LibraryProject/relationship_app/signals.py
+++ django-models/LibraryProject/relationship_app/signals.py
@@ -1,9 +1,8 @@
-from django.db.models.signals import post_save
-from django.dispatch import receiver
-from django.contrib.auth.models import User
-from .models import UserProfile
+from django.apps import AppConfig
 
-@receiver(post_save, sender=User)
-def create_user_profile(sender, instance, created, **kwargs):
-    if created:
-        UserProfile.objects.create(user=instance, role='Member')  # default role
+class RelationshipAppConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'relationship_app'
+
+    def ready(self):
+        import relationship_app.signals  # <--- this line ensures your signal is registered
