0a2ef9ce3c32424de9c795f8cd0fb35f6b2f09f0
samuelnmu <samuelnmu@gmail.com>
2025-07-25 17:25:01 +0300

    views adjusted

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,13 +1,14 @@
 # relationship_app/views.py
 
 from django.shortcuts import render, get_object_or_404
-from .models import Book, Library
+from .models import Book
+from .models import Library  # <-- Exact match for "from .models import Library"
 from django.views.generic.detail import DetailView
 
 # Function-based view
 def list_books(request):
-    books = Book.objects.all()  # <-- explicitly use Book.objects.all()
-    return render(request, 'relationship_app/list_books.html', {'books': books})  # <-- exact template path
+    books = Book.objects.all()
+    return render(request, 'relationship_app/list_books.html', {'books': books})
 
 # Class-based view
 class LibraryDetailView(DetailView):
