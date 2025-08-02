9fe6aa64d1c5fcf22d1a87976c711a98c2bb51eb
samuelnmu <samuelnmu@gmail.com>
2025-08-02 21:09:15 +0300

    Book added Meta

--- django-models/LibraryProject/relationship_app/models.py
+++ django-models/LibraryProject/relationship_app/models.py
@@ -34,6 +34,14 @@ class Book(models.Model):
     """
     title = models.CharField(max_length=200)
     author = models.ForeignKey(Author, on_delete=models.CASCADE, related_name='authors')
+    
+    class Meta:
+        # Custom permissions for the Book model
+        permissions = [
+            ("can_add_book", "Can add a book"),
+            ("can_change_book", "Can change a book"),
+            ("can_delete_book", "Can delete a book"),
+        ]
 
     def __str__(self):
         return self.title
