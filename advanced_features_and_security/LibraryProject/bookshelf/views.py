28767a3aa4317daaa2adbce256987ccd7ef7d097
samuelnmu <samuelnmu@gmail.com>
2025-08-02 22:54:13 +0300

    Custom Permissions

--- advanced_features_and_security/LibraryProject/bookshelf/views.py
+++ advanced_features_and_security/LibraryProject/bookshelf/views.py
@@ -27,7 +27,7 @@ Usage:
 
 # View Books — requires can_view
 @permission_required('bookshelf.can_view', raise_exception=True)
-def list_books(request):
+def book_list(request):
     books = Book.objects.all()
     return render(request, 'bookshelf/list_books.html', {'books': books})
 
