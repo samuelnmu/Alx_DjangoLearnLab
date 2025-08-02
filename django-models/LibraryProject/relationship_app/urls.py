b0b531c8f793d7a7203d39e60680a4895745e962
samuelnmu <samuelnmu@gmail.com>
2025-08-02 19:25:11 +0300

    CBV and FBV

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -1,3 +1,10 @@
 from django.urls import path
+from .views import LibraryDetailView
 from . import views
 
+urlpatterns = [
+    #function based views
+    path('books/',views.list_books,name='list-books'),
+    #class based views
+    path('library/<int:pk>/', LibraryDetailView.as_view(), name='library-detail'),
+]
---
+++ django-models/LibraryProject/relationship_app/views.py
@@ -0,0 +1,17 @@
+from .models import Book, Library
+from django.views.generic.detail import DetailView
+from django.shortcuts import render
+#lists all books stored in the database.
+
+# Function-based view: List all books
+def list_books(request):
+    books = Book.objects.all()
+    return render(request, 'list_books.html', {'books': books})
+
+#display details for a specific library, listing all books available in that library.
+
+class LibraryDetailView(DetailView):
+    model = Library
+    template_name = 'library_detail.html'  # Template file
+    context_object_name = 'library'  # The name used in the template
+
