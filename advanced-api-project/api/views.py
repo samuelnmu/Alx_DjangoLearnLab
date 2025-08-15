742c4913ed594b1bce944cca1f62043175663e81
samuelnmu <samuelnmu@gmail.com>
2025-08-15 14:30:46 +0300

    filtering, searching, and ordering capabilities

--- advanced-api-project/api/views.py
+++ advanced-api-project/api/views.py
@@ -6,6 +6,7 @@ from rest_framework import serializers
 from rest_framework.permissions import IsAuthenticatedOrReadOnly, IsAuthenticated
 from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework import filters
+from django_filters import rest_framework as filters
 
 
 
