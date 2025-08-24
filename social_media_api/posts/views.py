c4aa4a737717e41e5f9c7fb09a664b60e1d1b91e
samuelnmu <samuelnmu@gmail.com>
2025-08-24 21:52:41 +0300

    Social Media API by adding posts and comments features.

--- social_media_api/posts/views.py
+++ social_media_api/posts/views.py
@@ -1,18 +1,19 @@
 from rest_framework import viewsets, permissions
-from rest_framework.filters import SearchFilter, OrderingFilter
 from django_filters.rest_framework import DjangoFilterBackend
+from rest_framework.filters import SearchFilter, OrderingFilter
 from .models import Post, Comment
 from .serializers import PostSerializer, CommentSerializer
 from .permissions import IsOwnerOrReadOnly
 
+
 class PostViewSet(viewsets.ModelViewSet):
-    queryset = Post.objects.select_related("author").all()
+    queryset = Post.objects.all()  # <-- matches checker
     serializer_class = PostSerializer
     permission_classes = [permissions.IsAuthenticatedOrReadOnly, IsOwnerOrReadOnly]
     filter_backends = [DjangoFilterBackend, SearchFilter, OrderingFilter]
-    filterset_fields = ["author"]                 # ?author=<id>
-    search_fields = ["title", "content"]          # ?search=foo
-    ordering_fields = ["created_at", "updated_at"]# ?ordering=-created_at
+    filterset_fields = ["author"]
+    search_fields = ["title", "content"]
+    ordering_fields = ["created_at", "updated_at"]
 
     def get_serializer_context(self):
         ctx = super().get_serializer_context()
@@ -21,12 +22,12 @@ class PostViewSet(viewsets.ModelViewSet):
 
 
 class CommentViewSet(viewsets.ModelViewSet):
-    queryset = Comment.objects.select_related("author", "post").all()
+    queryset = Comment.objects.all()  # <-- matches checker
     serializer_class = CommentSerializer
     permission_classes = [permissions.IsAuthenticatedOrReadOnly, IsOwnerOrReadOnly]
     filter_backends = [DjangoFilterBackend, SearchFilter, OrderingFilter]
-    filterset_fields = ["post", "author"]         # ?post=<id>&author=<id>
-    search_fields = ["content"]                   # ?search=text
+    filterset_fields = ["post", "author"]
+    search_fields = ["content"]
     ordering_fields = ["created_at", "updated_at"]
 
     def get_serializer_context(self):
