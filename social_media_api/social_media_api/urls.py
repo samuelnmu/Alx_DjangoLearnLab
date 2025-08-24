eee498bb59ad0f08beb7e9f425f62eebea2be597
samuelnmu <samuelnmu@gmail.com>
2025-08-24 21:51:19 +0300

    Social Media API by adding posts and comments features.

--- social_media_api/social_media_api/urls.py
+++ social_media_api/social_media_api/urls.py
@@ -20,5 +20,5 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/accounts/', include('accounts.urls')),
-    path('api/', include('posts/urls')),  # <— posts & comments endpoints
+    path('api/', include('posts.urls')),  # <— posts & comments endpoints
 ]
