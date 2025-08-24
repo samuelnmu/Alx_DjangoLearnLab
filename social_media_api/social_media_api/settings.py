c1ed3efca0eab10d7ad2a9238de6c3556a1bc273
samuelnmu <samuelnmu@gmail.com>
2025-08-24 22:22:08 +0300

    Production

--- social_media_api/social_media_api/settings.py
+++ social_media_api/social_media_api/settings.py
@@ -132,6 +132,9 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
+# Define where collected static files will be stored in production
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
