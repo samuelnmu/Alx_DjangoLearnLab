c5de9336c5ae347b475785a569c426160f9bda25
samuelnmu <samuelnmu@gmail.com>
2025-08-24 21:39:01 +0300

    Social Media API from scratch, focusing on setting up the project environment, user authentication, and creating the initial user models.

---
+++ social_media_api/accounts/views.py
@@ -0,0 +1,33 @@
+from rest_framework import generics, permissions
+from rest_framework.authtoken.views import ObtainAuthToken
+from rest_framework.authtoken.models import Token
+from rest_framework.response import Response
+from django.contrib.auth import get_user_model
+
+from .serializers import RegisterSerializer, UserSerializer
+
+User = get_user_model()
+
+class RegisterView(generics.CreateAPIView):
+    queryset = User.objects.all()
+    serializer_class = RegisterSerializer
+
+    def create(self, request, *args, **kwargs):
+        response = super().create(request, *args, **kwargs)
+        user = User.objects.get(username=response.data["username"])
+        token, created = Token.objects.get_or_create(user=user)
+        return Response({"user": response.data, "token": token.key})
+
+class LoginView(ObtainAuthToken):
+    def post(self, request, *args, **kwargs):
+        response = super().post(request, *args, **kwargs)
+        token = Token.objects.get(key=response.data['token'])
+        return Response({"token": token.key, "user_id": token.user_id, "username": token.user.username})
+
+class ProfileView(generics.RetrieveUpdateAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+    def get_object(self):
+        return self.request.user
